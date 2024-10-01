110ac7652a09fefa828efd2e8ff82f38652d943b
hanan1193 <114731853+hanan1193@users.noreply.github.com>
2024-10-01 09:18:01 +0300

    Update problem#9.cpp

--- problem#9.cpp
+++ problem#9.cpp
@@ -1,1 +1,32 @@
-
+//https://leetcode.com/problems/container-with-most-water/
+//the link of the problem.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int maxArea(vector<int>&height){
+        int l=0,r=height.size()-1,ans;
+        int maximum=-1;
+        while(l<r){
+            ans=min(height[l],height[r])*(r-l);
+            maximum=max(maximum,ans);
+            if(height[l]<height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return maximum;
+        
+        
+    }
+};
+int main()
+{
+    vector<int>height = {1,8,6,2,5,4,8,3,7};
+    Solution s;
+    int result=s.maxArea(height);
+    cout<<result<<endl;
+    return 0;
+}
